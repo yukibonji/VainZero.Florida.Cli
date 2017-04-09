7e270ffd1a523acf18d07f67be6f16a5167519cf
vain0 <id_vain@yahoo.co.jp>
2017-04-09 12:58:34 +0900

    Add a file

--- VainZero.Florida.Core/Reports.DailyReport.fs
+++ VainZero.Florida.Core/Reports.DailyReport.fs
@@ -14,20 +14,6 @@ open VainZero.Florida.Data
 open VainZero.Florida.UI.Notifications
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Work =
-  let create project content duration note =
-    {
-      案件 =
-        project
-      内容 =
-        content
-      工数 =
-        duration
-      備考 =
-        note
-    }
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module DailyReport =
   let create works plan note =
     {

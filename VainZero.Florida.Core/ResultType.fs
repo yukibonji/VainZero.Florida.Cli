4453b7f14131ecf046a7701fa0a080c01e9b43ef
vain0 <id_vain@yahoo.co.jp>
2017-04-11 23:45:57 +0900

    Add Result type and module (defined in F# 4.1 or later)

---
+++ VainZero.Florida.Core/ResultType.fs
@@ -0,0 +1,29 @@
+﻿namespace global
+
+type Result<'x, 'e> =
+  | Ok of 'x
+  | Error of 'e
+
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Result =
+  let map (f: 'x -> 'y) (result: Result<'x, 'e>): Result<'y, 'e> =
+    match result with
+    | Ok x ->
+      Ok (f x)
+    | Error e ->
+      Error e
+
+  let mapError (f: 'e -> 'f) (result: Result<'x, 'e>): Result<'x, 'f> =
+    match result with
+    | Ok x ->
+      Ok x
+    | Error e ->
+      Error (f e)
+
+  let bind (f: 'x -> Result<'y, 'e>) (result: Result<'x, 'e>): Result<'y, 'e> =
+    match result with
+    | Ok x ->
+      f x
+    | Error e ->
+      Error e

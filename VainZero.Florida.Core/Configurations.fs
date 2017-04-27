1f9bcbe89dff924a2f93bacfe19cb0726b14ef25
vain0 <id_vain@yahoo.co.jp>
2017-04-27 14:29:51 +0900

    Remove an unused variable

--- VainZero.Florida.Core/Configurations.fs
+++ VainZero.Florida.Core/Configurations.fs
@@ -8,30 +8,6 @@ open VainZero.Florida.UI.Notifications
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Config =
-  let private empty =
-    {
-      RootDirectory =
-        "data"
-      Department =
-        None
-      UserName =
-        None
-      DailyReportSubmitConfig =
-        None
-      WeeklyReportConfig =
-        {
-          MeetingDay =
-            DayOfWeek.Friday
-        }
-      TimeSheetConfig =
-        {
-          DefaultFirstTime =
-            TimeSpan(9, 0, 0)
-          DefaultRecess =
-            TimeSpan(1, 0, 0)
-        }
-    }
-
   let private configPath = @"./data/config.yaml"
   let private configTemplatePath = @"./data/config-template.yaml"
 
